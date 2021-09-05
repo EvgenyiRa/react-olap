988ded95fa00d48d63f8dcd8cc1967793412d4ee
e.rassadnikov <e.rassadnikov@rosvodokanal.ru>
2021-09-05 09:36:55 +0300

    Создание формы администрирования для СУБД MYSQL. Добавления компонента для создания таблиц на основе данных из БД.

---
+++ src/components/TableSQL.js
@@ -0,0 +1,185 @@
+import React from 'react';
+import BootstrapTable from 'react-bootstrap-table-next';
+import {getParamForSQL,getParamDiff,getSQLRun,getDBType} from '../system.js';
+
+import $ from 'jquery'
+
+class TableSQL extends React.Component {
+    constructor(props) {
+        super(props);
+        this.getRowsBySQL = this.getRowsBySQL.bind(this);
+        this.state = {
+          rows: [],
+          selectRow:[],
+          selectRowFull:[],
+        };
+        this.pr_tableSQLdopAction_vis=false;
+        this.ul_oda=undefined;
+        this.panelScroll=() => {
+          if (this.pr_tableSQLdopAction_vis) {
+            var li_v=$(this.ul_oda).closest('li'),
+                this_top=$(li_v).offset().top+30,
+                this_left=$(li_v).offset().left+3;
+            $(this.ul_oda).css({'left':this_left+'px','top':this_top+'px'});
+          }
+       }
+       this.dbtype=getDBType();
+    }
+
+    getRowsBySQL() {
+      const thisV=this;
+      if (!!this.props.obj.sql) {
+        var data = {};
+        if (this.dbtype==='mysql') {
+            data.params=[];
+        }
+        else {
+            data.params={};
+        }
+        data.sql=thisV.props.obj.sql;
+        getParamForSQL(thisV.props.obj.paramGroup,thisV.props.obj.parParentID,data);
+        //console.log(parSQL);
+        getSQLRun(data,(response)=> {
+                          thisV.setState({rows: response.data});
+                          if (!!thisV.props.obj.afterLoadRows) {
+                              thisV.props.obj.afterLoadRows(thisV,response.data);
+                          }
+                      },
+                 this.props.obj.stateLoadObj
+               );
+
+      }
+    }
+
+
+    componentDidMount() {
+        if (!!!this.props.obj.parParentID) {
+            this.getRowsBySQL();
+        }
+        let thisV=this;
+        if (!!this.props.obj.filterFactory) {
+          //добавляем кнопку поиска, по-умолчанию скрываем все поля фильтрации
+          var b_tab=$('div.react-bootstrap-table table#'+this.props.obj.tab_id).closest('div');
+          if ($(b_tab).find('button.react-bootstrap-table-but-searche').length===0) {
+            var bt_label=$(b_tab).find('th label.filter-label');
+            $(b_tab).append('<button class="react-bootstrap-table-but-searche" type="button">&#128269;</button>');
+            $(bt_label).hide();
+            $(b_tab).on("click", "button.react-bootstrap-table-but-searche", function() {
+              if ($(bt_label).first().is(':visible')) {
+                  $(bt_label).hide();
+              }
+              else {
+                  $(bt_label).show();
+              }
+            });
+          }
+        }
+        //Добавляем панель действий если есть подъодящие элементы
+        if ((!!this.props.obj.addRow) || (!!this.props.obj.deleteRow)
+            || (!!this.props.obj.dopAction) || (!!this.props.obj.editRow)) {
+          var b_tab=$('div.react-bootstrap-table table#'+this.props.obj.tab_id).closest('div');
+          $(b_tab).prepend('<ul class="ul_cons top-level" style="margin:0.3em 0 0.3em 0;padding: 0;">');
+          var ul_v=$(b_tab).find('ul.ul_cons.top-level');
+          if (!!this.props.obj.addRow) {
+              $(ul_v).append(`<li id="`+this.props.obj.tab_id+`" class="li_cons li_cons_top tableSQLaddRow" style="background: none;">
+                                  <a id="`+this.props.obj.tab_id+`" title="Добавить строку" class="tableSQLaddRow">
+                                      <img src="`+require('../img/add.png').default+`" style="height:1.8em;width:auto;">
+                                  </a>
+                              </li>`);
+              $(ul_v).find('li.tableSQLaddRow').click(()=>thisV.props.obj.addRow(thisV));
+          }
+          if (!!this.props.obj.editRow) {
+              $(ul_v).append(`<li id="`+this.props.obj.tab_id+`" class="li_cons li_cons_top tableSQLeditRow" style="background: none;">
+                                  <a id="`+this.props.obj.tab_id+`" title="Редактировать строку" class="tableSQLeditRow">
+                                      <img src="`+require('../img/edit.png').default+`" style="height:1.8em;width:auto;">
+                                  </a>
+                              </li>`);
+              $(ul_v).find('li.tableSQLeditRow').click(()=>thisV.props.obj.editRow(thisV));
+          }
+          if (!!this.props.obj.deleteRow) {
+              $(ul_v).append(`<li id="`+this.props.obj.tab_id+`" class="li_cons li_cons_top tableSQLdeleteRow" style="background: none;">
+                                  <a id="`+this.props.obj.tab_id+`" title="Удалить строку" class="tableSQLdeleteRow">
+                                      <img src="`+require('../img/rep_del.png').default+`" style="height:1.8em;width:auto;">
+                                  </a>
+                              </li>`);
+              $(ul_v).find('li.tableSQLdeleteRow').click(()=>thisV.props.obj.deleteRow(thisV));
+          }
+          if (!!this.props.obj.dopAction) {
+              $(ul_v).append(`<li id="`+this.props.obj.tab_id+`" class="li_cons li_cons_top tableSQLdopActionBlok" style="background: none;">
+                                <img src="`+require('../img/actions.png').default+`" style="height:1.8em;width:auto;" title="Доп.действия">
+                                <ul class="tableSQLdopAction ul_cons second-level"></ul>
+                              </li>`);
+
+              $(ul_v).find('li.li_cons.li_cons_top.tableSQLdopActionBlok[id="'+this.props.obj.tab_id+'"]').click(function() {
+                  thisV.ul_oda=$(this).find('ul.tableSQLdopAction');
+                  if (!$(thisV.ul_oda).is(':visible')) {
+                      $(thisV.ul_oda).show();
+                      var this_top=$(this).offset().top+30,
+                          this_left=$(this).offset().left+3;
+                      $(thisV.ul_oda).css({'left':this_left+'px','top':this_top+'px'});
+                      thisV.pr_tableSQLdopAction_vis=true;
+                  }
+                  else {
+                      $(thisV.ul_oda).hide();
+                      thisV.pr_tableSQLdopAction_vis=false;
+                  }
+              });
+
+              //$('div#root').on('scroll','main',this.panelScroll);
+              $('main').bind('scroll', this.panelScroll);
+
+              var ul_sl=$(ul_v).find('ul.tableSQLdopAction.ul_cons.second-level');
+              this.props.obj.dopAction.forEach((item) => {
+                $(ul_sl).append(`<li class="li_cons tableSQLdopAction" id="`+item.id+`">
+                                    <a id="`+item.id+`">`+item.label+`</a>
+                                 </li>`);
+                $(ul_sl).find('li.tableSQLdopAction[id="'+item.id+'"]').click(()=>item.callback(thisV));
+              })
+          }
+      }
+      if (!!this.props.obj.componentDidMount) {
+          this.props.obj.componentDidMount(this);
+      }
+    }
+
+    componentWillUnmount() {
+      if (!!this.props.obj.dopAction) {
+          $('main').unbind('scroll', this.panelScroll);
+      }
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        // Популярный пример (не забудьте сравнить пропсы):
+        //console.log(this.props);
+        if (getParamDiff(this.props.obj.paramGroup,prevProps.obj.paramGroup,this.props.obj.parParentID,this.props.obj.tabEventKey)) {
+            this.getRowsBySQL();
+        }
+      }
+
+    render() {
+      const NoDataIndication = () => (
+        <div className="spinner">
+          Отсутствуют данные для отображения
+        </div>
+      );
+        return (
+              <BootstrapTable
+                id={ this.props.obj.tab_id }
+                data={this.state.rows}
+                keyField={ this.props.obj.keyField}
+                columns={ this.props.obj.columns }
+                classes={ this.props.obj.tableContainerClass}
+                bodyClasses={ this.props.obj.bodyClasses}
+                selectRow={this.props.obj.selectRowProp }
+                pagination={ (!!this.props.obj.paginationFactory)? this.props.obj.paginationFactory( (!!this.props.obj.paginationOptions) ? this.props.obj.paginationOptions:undefined ):undefined }
+                noDataIndication={ () => <NoDataIndication /> }
+                filter={ (!!this.props.obj.filterFactory) ? this.props.obj.filterFactory((!!this.props.obj.filterFactoryIn) ? this.props.obj.filterFactoryIn:undefined):undefined }
+                rowEvents={this.props.obj.rowEvents}
+              />
+
+
+        );
+    }
+}
+
+export default TableSQL;
---
+++ src/forms/admin/UsersMYSQL.js
@@ -0,0 +1,186 @@
+import React,{ useState,useRef } from 'react';
+
+import Loading from '../../components/Loading';
+import AlertPlus from '../../components/AlertPlus';
+import ConfirmPlus from '../../components/ConfirmPlus';
+import WinModal from '../../components/WinModal';
+import TableOLAP from '../../components/TableOLAP';
+import BootstrapInput from '../../components/BootstrapInput';
+import MultiselectSQL from '../../components/MultiselectSQL';
+import TableSQL from '../../components/TableSQL';
+import Container from 'react-bootstrap/Container';
+import paginationFactory from 'react-bootstrap-table2-paginator';
+import Checkbox from '@material-ui/core/Checkbox';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import {getDBType,getSQLRun} from '../../system.js';
+/*import { format,startOfMonth } from 'date-fns';*/
+
+import $ from 'jquery';
+
+function Users() {
+  const dbType=getDBType();
+  //хук для отслеживания изменения параметров компонетов (для упрощения взаимодействия компонентов)
+  let [paramGroupV, setParamGroupV] = useState({users:[]});
+  //хук-ссылки на элементы для удобной работы с ними
+  const refAlertPlus=useRef(),
+        refConfirmPlus=useRef(),
+        refLoading=useRef(),
+        refTableSQL=useRef(),
+        refWinModal=useRef();
+
+  const tabUser='table#tab1 tbody';
+
+  //объект для выпадающего списка с данными из БД
+  const selectUserObj={stateLoadObj:refLoading,
+                        label:'Пользователи',
+                        paramGroup:paramGroupV,
+                        setParamGroup:setParamGroupV,
+                        //наименование параметра для зависимых(дочерних) элементов
+                        parChealdID:"users",
+                        //необходимо наличие двух полей с именами value,label
+                        sql:`SELECT DISTINCT
+                                    R.USER_ID "value",
+                                    R.FIO "label"
+                                FROM REP_USERS R
+                                ORDER BY R.FIO`,
+                        multiple:true
+                       };
+
+//объект для таблицы с данными из БД
+const tableSQLObj={stateLoadObj:refLoading,
+   tableContainerClass:'max-content',
+   bodyClasses:'body_row_dblclick',
+   tab_id:"tab1",
+   paramGroup:paramGroupV,
+   parParentID:['users'],
+   sql:`SELECT U.FIO,
+               U.USER_ID,
+               U.LOGIN,
+               U.SOL,
+               U.EMAIL,
+               U.PHONE
+         FROM REP_USERS U
+         WHERE (U.USER_ID IN (:users) OR COALESCE(:users,-777)=-777)
+         ORDER BY U.LOGIN`,
+   afterLoadRows:(thisV)=>{
+       thisV.state.selectRowFull=[];
+       const newObj={...paramGroupV};
+       newObj.userTab=-777;
+       $(tabUser).find('tr.checked').removeClass('checked');
+       setParamGroupV(newObj);
+   },
+  rowEvents: {
+    onClick: (e, row, rowIndex) => {
+      //не устраивает работа selectRowProp-onSelect
+      const tr=$(e.target).closest('tr'),
+            newObj={...paramGroupV};
+      if ($(tr).hasClass('checked')) {
+         newObj.userTab=-777;
+         refTableSQL.current.state.selectRowFull=[];
+         $(tr).removeClass('checked');
+      }
+      else {
+        newObj.userTab=row[refTableSQL.current.props.obj.keyField];
+        refTableSQL.current.state.selectRowFull=row;
+        $(tabUser).find('tr.checked').removeClass('checked');
+        $(tr).addClass('checked');
+      }
+      setParamGroupV(newObj);
+    }
+  },
+  keyField:'USER_ID',
+  columns:[{dataField:'FIO',text:'ФИО',headerAttrs: (column, colIndex) => ({ 'width': `150px` })},
+           {dataField:'LOGIN',text:'Логин',headerAttrs: (column, colIndex) => ({ 'width': `110px` })},
+           {dataField:'EMAIL',text:'E-mail',headerAttrs: (column, colIndex) => ({ 'width': `110px` })},
+           {dataField:'PHONE',text:'Телефон',headerAttrs: (column, colIndex) => ({ 'width': `110px` })}
+        ],
+  //действия панели таблицы
+  addRow:(thisV) => {
+            refWinModal.current.setState(getWinModalUser('add'));
+         },
+  editRow:(thisV) => {
+          if (thisV.state.selectRowFull.length===0) {
+              refAlertPlus.current.setState({show:true,text:'Необходимо кликом левой кнопки мыши по строке таблицы выбрать пользователя'});
+          }
+          else {
+              refWinModal.current.setState(getWinModalUser('edit'));
+          }
+        },
+  deleteRow:(thisV) => {
+              if (thisV.state.selectRowFull.length===0) {
+                  refAlertPlus.current.setState({show:true,text:'Необходимо кликом левой кнопки мыши по строке таблицы выбрать пользователя'});
+              }
+              else {
+                  refConfirmPlus.current.setState({show:true,
+                                                    text:'Вы действительно хотите удалить пользователя с логином "'+thisV.state.selectRowFull['LOGIN']+'"',
+                                                    callback:(res) => {
+                                                        if (res) {
+                                                          let user_id=+thisV.state.selectRowFull['USER_ID'];
+                                                          if (!!user_id) {
+                                                              let data={};
+                                                              data.sql=`DELETE FROM REP_USERS
+                                                                         WHERE USER_ID=:user_id;
+                                                                        DELETE FROM REP_USERS_RIGHTS
+                                                                        WHERE USER_ID=:user_id;`;
+                                                              data.params=[user_id];
+                                                              getSQLRun(data,
+                                                                           function(response0) {
+                                                                              refAlertPlus.current.setState({show:true,text:'Выбранный пользователь успешно удалён'});
+                                                                              thisV.getDataSQL();
+                                                                              //обновление строк таблицы
+                                                                           },
+                                                                           refLoading
+                                                                         );
+                                                          }
+                                                        }
+                                                    }});
+              }
+           },
+   paginationFactory:paginationFactory,
+   paginationOptions:{paginationSize: 7,
+                       sizePerPageList: [{
+                           text: '10', value: 10
+                         }, {
+                           text: '50', value: 50
+                         }, {
+                           text: '100', value:100
+                         }, {
+                           text: '500', value:500
+                         }]
+                     },
+   componentDidMount:(thisV)=>{
+       thisV.getRowsBySQL();
+   }
+   //filterFactory:filterFactory,
+  };
+
+  //получение объекта модального окна для работы с пользователями и их правами (добавление,редактирование)
+  const getWinModalUser=(type) => {
+
+  }
+
+  return (
+    <div className="App">
+      <Loading ref={refLoading} />
+      <AlertPlus ref={refAlertPlus}/>
+      <ConfirmPlus ref={refConfirmPlus}/>
+      <Container fluid>
+        <Row>
+          <Col>
+            <MultiselectSQL obj={selectUserObj}/>
+          </Col>
+        </Row>
+        <Row style={{marginTop:'1rem'}}>
+          <Col>
+            <TableSQL ref={refTableSQL} obj={ tableSQLObj }/>
+            <WinModal ref={refWinModal}/>
+          </Col>
+        </Row>
+
+      </Container>
+    </div>
+  );
+}
+
+export default Users;
